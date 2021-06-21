f956d0da47ca48419ab73f7a75be80b419541368
Lauren Gifford <lauren.gifford.17@gmail.com>
2021-06-21 11:47:56 -0400

    changes props display

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,13 +9,12 @@ class QuizQuestion extends React.Component {
         return (
             <main>
                 <section>
-                <p>{this.props.quiz_question.instruction_text} </p>
-                {/* <p>{this.props.instruction_text} </p> */}
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
-                <ul>
-                    <li>{this.props.quiz_question.answer_options[0]}</li>
-                </ul>
+                    <ul>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
+                    </ul>
                 </section>
             </main>
         )
